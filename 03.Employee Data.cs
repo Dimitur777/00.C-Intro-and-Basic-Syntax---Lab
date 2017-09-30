b91c308e9d2ae634bbf5995e5e0f6c2fa332d112
Dimitar Lyolev <lyolevofficial@gmail.com>
2017-09-30 11:38:32 +0300

    Add files via upload

---
+++ 03.Employee Data.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int employeeId = int.Parse(Console.ReadLine());
+            decimal salary = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {employeeId:D8}");
+            Console.WriteLine($"Salary: {salary:F2}");
+
+
+        }
+    }
+}
